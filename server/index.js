fb8a733540ba34efab4e5268ac39248c822aeee5
Kaelynn Amodia <89933890+nnyleak@users.noreply.github.com>
2026-02-10 17:35:47 -0500

    connected db

--- server/index.js
+++ server/index.js
@@ -6,19 +6,20 @@ const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "book archive",
-    password: "Kjraxoxo_3",
-    port: 5432
+  user: "postgres",
+  host: "localhost",
+  database: "book archive",
+  password: "Kjraxoxo_3",
+  port: 5433,
 });
+db.connect();
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("hello from server!");
-})
+  res.send("hello from server!");
+});
 
 app.listen(port, () => {
-    console.log(`server running on port ${port}`);
-})
+  console.log(`server running on port ${port}`);
+});
